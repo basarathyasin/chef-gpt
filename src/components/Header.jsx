075ff733e07c0c62370f854f3a0b33b759ac70ab
Basarath Yasin <bbox@Basaraths-MacBook-Pro.local>
2025-10-13 14:47:31 +0530

    Remove ClassName

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from "/Users/bbox/cheif-gpt/src/assets/chief gpt logo.png"
+
 export function Header(){
     return(
-        <header className="header">
-            <img src="src/assets/chief gpt logo.png" alt="logo"/>
+        <header>
+            <img src={logo} alt="logo"/>
            <h1>Cheif GPT</h1> 
         </header>
     )
