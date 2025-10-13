d2e78cf5221f4685343ee4d253459b111c4ca1bc
Basarath Yasin <bbox@Basaraths-MacBook-Pro.local>
2025-10-13 15:36:14 +0530

    Add Form

--- src/components/MainComp.jsx
+++ src/components/MainComp.jsx
@@ -1,7 +1,14 @@
 export default function Main(){
     return(
         <main>
-
+            <form className="add-ingredient-form">
+                <input 
+                type="text"
+                placeholder="e.g. Potatoes"
+                aria-label="Add ingredients"
+                />
+                <button>Add ingredients</button>
+            </form>
         </main>
     )
 }
