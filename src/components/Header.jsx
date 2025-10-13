db584ff252fe3502a8206efaec05c7c40886f655
Basarath Yasin <bbox@Basaraths-MacBook-Pro.local>
2025-10-13 14:24:32 +0530

    Header component initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header(){
+    return(
+        <header className="header">
+            <img src="src/assets/chief gpt logo.png" alt="logo"/>
+           <h1>Cheif GPT</h1> 
+        </header>
+    )
+}
