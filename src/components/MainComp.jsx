1aad68091a83e232912c7fa8c9e590f7475c6f6a
Basarath Yasin <bbox@Basaraths-MacBook-Pro.local>
2025-10-14 14:10:17 +0530

    Add Form Listner & display value on screen

--- src/components/MainComp.jsx
+++ src/components/MainComp.jsx
@@ -1,14 +1,35 @@
-export default function Main(){
-    return(
+import React from "react"
+
+export default function Main() {
+
+    const[ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget);
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prev => [...prev, newIngredient]);
+    }
+    
+
+    return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
-                type="text"
-                placeholder="e.g. Potatoes"
-                aria-label="Add ingredients"
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button>Add ingredients</button>
+                <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
